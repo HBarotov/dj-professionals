70db4d1ac8e22ae49d0bd75e879cf6c64033ec51
HBarotov <barotovhojiakbar.me@gmail.com>
2024-07-20 14:02:39 +0500

    added easy-thumbnails

--- app/django_project/settings.py
+++ app/django_project/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount.providers.github",
     "debug_toolbar",
     "django_countries",
+    "easy_thumbnails",
     # Local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
